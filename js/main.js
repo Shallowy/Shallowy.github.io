2a454fa2817295b618ce9d79c906702a77c63cd9
Shallowy <2533136630@qq.com>
2019-10-05 20:35:35 +0800

    Site updated: 2019-10-05 20:35:33

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
-  // List of HTML entities for escaping.
-  var htmlEscapes = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#x27;',
-    '/': '&#x2F;'
-  };
-  // Regex containing the keys listed immediately above.
-  var htmlEscaper = /[&<>"'\/]/g;
-  // Escape a string for HTML interpolation.
-  escapeHTML = function(string) {
-    return ('' + string).replace(htmlEscaper, function(match) {
-      return htmlEscapes[match];
-    });
-  };
-  // Find Prism code snippet and escape markup
-  $('.lang-cpp').each(function() {
-    var codeHtml = $(this).html();
-    codeHtml = escapeHTML(codeHtml);
-    $(this).html(codeHtml);
-  });
+
 (function($) {
 
 	skel.breakpoints({
